ec481cb21145a31bbc33b689fbe06cdc3ec21933
ArkanFauzan <arkan@icreativelabs.com>
2025-05-18 20:20:17 +0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace StudentExportApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
